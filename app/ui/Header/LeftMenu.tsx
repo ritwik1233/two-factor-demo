fd649ebd13bc27bfde9a01cbaaecb01d4be13554
Ritwik Sinha <ritwiksinha@Ritwiks-MacBook-Air.local>
2024-09-25 20:53:58 +0530

    route fix

--- app/ui/Header/LeftMenu.tsx
+++ app/ui/Header/LeftMenu.tsx
@@ -4,30 +4,21 @@ import {
   List,
   ListItem,
   ListItemAvatar,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
   SwipeableDrawer,
   Typography,
 } from "@mui/material";
 import React from "react";
 import LogoutButton from "../Buttons/LogoutButton";
-import Link from "next/link";
-import { Settings, SmartToy } from "@mui/icons-material";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 interface LeftMenuProps {
   openSideNavigation: boolean;
   setOpenSideNavigation: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const LeftMenu: React.FC<LeftMenuProps> = (props) => {
-  const router = useRouter();
   const { openSideNavigation, setOpenSideNavigation } = props;
   const session: any = useSession();
   const user = session?.data?.user;
-  const onProfileClick = () => {
-    router.push("/profile");
-  };
+
   return (
     <SwipeableDrawer
       open={openSideNavigation}
@@ -47,7 +38,6 @@ const LeftMenu: React.FC<LeftMenuProps> = (props) => {
               <ListItem
                 sx={{ py: "1em", cursor: "pointer" }}
                 data-cy={"side-menu-profile"}
-                onClick={onProfileClick}
               >
                 <ListItemAvatar>
                   <Avatar src="" />
